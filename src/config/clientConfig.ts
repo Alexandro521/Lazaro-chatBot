5fa40d4790c9d580b9e900b1be83a2ce7ae35fe2
Alexandro mendez <133552055+Alexandro521@users.noreply.github.com>
2024-12-19 23:00:49 -0400

    Update clientConfig.ts

--- src/config/clientConfig.ts
+++ src/config/clientConfig.ts
@@ -9,11 +9,6 @@ export const clientConfig: ClientOptions = {
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      "--disable-accelerated-2d-canvas",
-      "--no-first-run",
-      "--no-zygote",
-      "--disable-gpu",
     ],
   },
   webVersionCache: {
